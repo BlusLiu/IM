6c164cec65f6802b4339b42f89db53b79e0b6e72
BlusLiu <l.zqy@qq.com>
2020-04-05 19:31:23 +0800

    适配了安卓9.0，安卓1.0相册获取失败
    网络问题，http请求默认不允许

---
+++ app/src/main/java/com/example/im2/AppFileProvider.java
@@ -0,0 +1,12 @@
+package com.example.im2;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * @Author: liuzhen
+ * @Description:
+ * @Date: Create in 11:52 2020/4/5
+ */
+public class AppFileProvider extends FileProvider {
+
+}
--- factory/src/main/java/com/example/factory/persenter/user/UpdateInfoPresenter.java
+++ factory/src/main/java/com/example/factory/persenter/user/UpdateInfoPresenter.java
@@ -1,6 +1,7 @@
 package com.example.factory.persenter.user;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.common.APP.Applocation;
 import com.example.factory.Factory;
@@ -42,8 +43,19 @@ public class UpdateInfoPresenter
                 @Override
                 public void run() {
                     String url = UploadHelper.uploadPortrait(photoFilePath);
+                    Log.e("PHOTO", "run: :" + photoFilePath + "//" + url);
                     if (TextUtils.isEmpty(url)){
-                        view.showError(R.string.data_upload_error);
+//                        view.showError(R.string.data_upload_error);
+                        Run.onUiAsync(new Action() {
+                            @Override
+                            public void call() {
+                                view.showError(R.string.data_upload_error);
+                            }
+                        });
+
+//                        UserUpdateModel model = new UserUpdateModel("", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586068248190&di=ca211dcf174951d12d3044261111f323&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20181229%2F4071d3af818b41fdb90c5a618dd5c190.jpeg", desc, isMan? 1: 2);
+//                        //上传默认头像
+//                        UserHelper.update(model, UpdateInfoPresenter.this);
                     }else {
                         UserUpdateModel model = new UserUpdateModel("", url, desc, isMan? 1: 2);
                         //上传
